5753e25292084762e65dc215f6f6d048bd0a986a
Eveline Vervliet <57260992+Eveline-97@users.noreply.github.com>
2023-09-14 19:44:14 +0200

    tonality, pitch and single note events

--- main.js
+++ main.js
@@ -6,9 +6,28 @@ const $queryField = document.getElementById('query');
 const $soundDiv = document.getElementById('sound-div');
 const $extraFilter = document.getElementById('extra-filter');
 const $deleteFilter = document.getElementById('delete-filter');
+const $downloadAll = document.getElementById('download-files');
 
 /*FILTERS*/
 let filters = [
+    {
+        name: 'tonality',
+        rootNote: 'none',
+        scale: 'none',
+        min: 0,
+        max: 100,
+        shown: false //only true if single event is not ticked
+    },
+    {
+        name: 'pitch',
+        rootNote: 'none',
+        octave: 'none',
+        shown: false //only true if single event is ticked
+    },
+    {
+        name: 'single-event',
+        shown: false
+    },
     {
         name: 'brightness',
         min: 0,
@@ -53,6 +72,75 @@ let filters = [
     }
 ]
 
+/*SINGLE EVENT*/
+const $singleEvent = document.getElementById('single-event');
+const $tonalityDiv = document.getElementById('tonality-div');
+const $pitchDiv = document.getElementById('note-name-div');
+let singleEvent = false;
+
+$('#single-event').change(() => {
+    singleEvent = $singleEvent.checked;
+    checkSingleEvent();
+})
+
+function checkSingleEvent() {
+    filters[2].shown = singleEvent;
+    if (singleEvent) {
+        //hide tonality div, because it has no use with a single event
+        $tonalityDiv.style.display = 'none';
+        filters[0].shown = false;
+        //show pitch div
+        $pitchDiv.style.display = 'block';
+    } else {
+        //hide pitch div, because it only has a use with a single event
+        $pitchDiv.style.display = 'none';
+        filters[1].shown = false;
+        //show tonality div
+        $tonalityDiv.style.display = 'block';
+    }
+}
+checkSingleEvent();
+
+/*TONALITY and PITCH*/
+const $rootNote = document.getElementById('root-note');
+const $scale = document.getElementById('scale');
+const $pitch = document.getElementById('pitch');
+const $octave = document.getElementById('octave');
+
+/*tonality*/
+$('#root-note').change(() => {
+    filters[0].rootNote = $rootNote.value;
+    //include Tonality confidence range
+    if ($rootNote.value == 'none') {
+        filters[0].shown = false;
+    } else {
+        filters[0].shown = true;
+    }
+})
+
+$('#scale').change(() => {
+    filters[0].scale = $scale.value;
+    console.log(filters[0]);
+})
+
+/*pitch*/
+$('#pitch').change(() => {
+    filters[1].rootNote = $pitch.value;
+    if ($pitch.value == 'none') {
+        filters[1].shown = false;
+    } else {
+        filters[1].shown = true;
+    }
+    console.log(filters[1]);
+})
+$('#octave').change(() => {
+    filters[1].octave = $octave.value;
+    console.log(filters[1]);
+})
+
+/*RANGE FILTERS*/
+
+//sliders
 filters.forEach(filter => {
     $('#' + filter.name + '-range').slider({
         range: true,
@@ -67,11 +155,11 @@ filters.forEach(filter => {
     })
 })
 
-/*display filters*/
+//display the range sliders when chosen
 let displayedFilters = [];
 
 function filterChange(filter) {
-    let number = filter.id.replace('filter', '')-1;
+    let number = filter.id.replace('filter', '') - 1;
 
     let currentFilter = null;
     filter.addEventListener('change', () => {
@@ -97,9 +185,10 @@ function filterChange(filter) {
     })
 };
 
+//for first filter
 filterChange(document.getElementById('filter1'));
 
-/*extra filter*/
+//add or remove filter
 let currentAmountOfFilters = 1;
 $extraFilter.addEventListener('click', () => {
     let newFilter =
@@ -126,9 +215,9 @@ $extraFilter.addEventListener('click', () => {
 $deleteFilter.addEventListener('click', () => {
     $(`#filter${currentAmountOfFilters}`).remove();
     $(`#filter${currentAmountOfFilters}-label`).remove();
-    
+
     //make selected filter invisible
-    let currentFilter = displayedFilters[currentAmountOfFilters-1];
+    let currentFilter = displayedFilters[currentAmountOfFilters - 1];
     document.getElementById(currentFilter + 'Div').style.display = 'none';
     let obj = filters.find(o => o.name === currentFilter);
     obj.shown = false;
@@ -136,20 +225,39 @@ $deleteFilter.addEventListener('click', () => {
     currentAmountOfFilters--;
 })
 
-/*search*/
+/*SEARCH*/
 let foundSounds = [];
 let soundList = [];
+let links = [];
+
 let filterString = `ac_tonality:[B major] ac_tonality_confidence:[0.8 TO 1]`;
 
 /*license: 
 "http://creativecommons.org/publicdomain/zero/1.0/"*/
 
 $searchButton.addEventListener('click', function () {
-    filterString = `ac_tonality:[B major] ac_tonality_confidence:[0.8 TO 1]`;
+    filterString = '';
     filters.forEach(filter => {
         //only add those filters that are shown
         if (filter.shown) {
-            filterString += ` ac_${filter.name}:[${filter.min} TO ${filter.max}]`;
+            if (filter.name == 'tonality') {
+                //special case for tonality, because 0 to 1 not 0 to 100
+                if (filter.rootNote == 'none' || filter.scale == 'none') {
+                    alert('Please provide all necessary information for the tonality');
+                }
+                filterString += `ac_${filter.name}:[${filter.rootNote} ${filter.scale}] `;
+                filterString += `ac_tonality_confidence:[${filter.min / 100} TO ${filter.max / 100}] `;
+            } else if (filter.name == 'pitch') {
+                //special case for pitch
+                if (filter.rootNote == 'none' || filter.octave == 'none') {
+                    alert('Please provide all necessary information for the pitch');
+                }
+                filterString += `ac_note_name:${filter.rootNote}${filter.octave} `;
+            } else if (filter.name == 'single-event') {
+                filterString += `ac_single_event:true `;
+            } else {
+                filterString += ` ac_${filter.name}:[${filter.min} TO ${filter.max}] `;
+            }
         }
     });
     console.log(filterString);
@@ -163,29 +271,62 @@ $searchButton.addEventListener('click', function () {
             sounds.results.forEach(sound => {
                 foundSounds.push(sound);
             });
+            deleteAllSounds();
             displaySounds(foundSounds);
         },
         errorMsg
     );
+    console.log(foundSounds);
 })
 
 function displaySounds(arr) {
-    arr.forEach(sound => {
-        console.log(sound);
+    soundList = [];
+
+    //now get sounds
+    arr.forEach((sound, index) => {
         freesound.getSound(
             sound.id,
             result => {
+                foundSounds[index].src = result.url;
+
                 snd = new Audio(result.previews['preview-hq-mp3']);
                 snd.controls = true;
                 document.getElementById('sound-div').appendChild(snd);
                 snd.play();
                 soundList.push(snd);
+
+                //create link for each sound
+                let link = document.createElement('a');
+                link.id = `link-for-${sound.id}`;
+                link.style.display = 'none';
+                link.setAttribute('href', result.name);
+                links[index] = link;
+
+                document.body.appendChild(link);
             },
             errorMsg
         )
     })
+
+    console.log(soundList);
+}
+
+
+function deleteAllSounds() {
+    $('#sound-div').remove();
+    let newDiv = `<div id="sound-div"></div>`
+    $('footer').before(newDiv);
 }
 
 function errorMsg() {
-    alert('Your search was invalid. Please try again.');
+    alert('Your search was invalid or the audio files could not be found. Please try again later.');
 }
+
+//download al audio files
+$downloadAll.addEventListener('click', () => {
+    let sounds = document.getElementsByTagName('audio');
+    for (let i = 0; i < sounds.length; i++) {
+        console.log(sounds[i]);
+        sounds[i].download();
+    }
+})
